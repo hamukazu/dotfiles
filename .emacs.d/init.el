e5ba2259911a38a9f91fbc83205abc7f3265927c
Kimikazu Kato <kato@SET51212-kt.(none)>
2013-04-18 19:16:37 +0900

    modified:   .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,63 +1,36 @@
-;;; First of all...(global-font-lock-mode t)
+;;; First of all...
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "C-; g") 'goto-line)
 (recentf-mode)
 (setq visible-bell t)
 (setq truncate-partial-width-windows nil)
 (setq-default indent-tabs-mode nil)
-(set-language-environment "Japanese")
-(require 'ruby-mode)
-(defun ruby-mode-set-encoding () ())
-
-
-;; set default coding system
 (set-default-coding-systems 'utf-8)
+(setq inhibit-startup-message t)
 
+;; Packages
+(require 'package)
+(setq package-archives 
+      (append '(("marmalade" . "http://marmalade-repo.org/packages/")
+                ("melpa" . "http://melpa.milkbox.net/packages/"))
+              package-archives))
+(package-initialize)
 
-;; evil-mode
-(setq load-path (cons "~/.emacs.d/plugins/evil-evil" load-path))
-(require 'evil)
-
-;; for anthy
-;(load-library "anthy")
-;(setq default-input-method "japanese-anthy")
-;(global-set-key [zenkaku-hankaku] 'toggle-input-method)
 ;; for mozc
 (require 'mozc)
-(setq default-input-method "japanese-mozc");;; 
-(setq load-path (cons "~/.emacs.d/plugins" load-path))
-(setq load-path (cons "~/.emacs.d/auto-install" load-path))
-(global-set-key (kbd "<zenkaku-hankaku>") 'toggle-input-method)
-
-;; for ghc
-(setq load-path (cons "~/cabal/ghc-mod-1.10.12" load-path))
-(setq exec-path (cons "~/cabal/bin" exec-path))
-
+(setq default-input-method "japanese-mozc")
 
 ;; color theme
-(setq load-path (cons "~/.emacs.d/plugins/color-theme-6.6.0" load-path))
 (require 'color-theme)
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
      (color-theme-euphoria)))
 
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-;;; Additional mode
-;(load "cuda-mode.el")
-(autoload 'js2-mode "js2" nil t)
-(setq inhibit-startup-message t)
-(setq auto-mode-alist (append '(("\\.ml" . tuareg-mode) 
-                                ("\\.html" . html-helper-mode)
-                                ("\\.hs" . haskell-mode)
-                                ("wscript" . python-mode)
-                                ("\\.tex$" . yatex-mode)
-                                ("js" . js2-mode))
-
-                              auto-mode-alist))
-
-;(autoload 'ghc-init "ghc" nil t)
-;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;;; Screen size specific configuration
+(setq initial-frame-alist 
+      (append '((width . 85)
+		(height . 50)) initial-frame-alist))
 
 ;;; Template
 (require 'autoinsert)
@@ -65,7 +38,6 @@
 (setq auto-insert-alist 
       (append '(("\\.cpp$" . ["template.cpp" my-template])
                 ("\\.h$" . ["template.h" my-template]))))
-
 (defvar template-replacements-alists
   '(("%file%" . (lambda () (file-name-nondirectory (buffer-file-name))))
     ("%file-noext%" . 
@@ -83,94 +55,19 @@
   (message "done."))
 (add-hook 'find-file-not-found-hooks 'auto-insert)
 
-;;; yasnippet
-
-(add-hook 'html-helper-timestamp-hook (function (lambda ()
-	   (insert "Last modified: " (format-time-string "%Y-%m-%d %T\n")))))
-
 ;;; C-mode
 
 (setq common-function-for-c-hook
          (lambda () 
            (local-set-key (kbd "C-; c") 'compile)
            (setq compile-command "./waf")))
-
 (add-hook 'c-mode-hook common-function-for-c-hook)
 (add-hook 'c++-mode-hook common-function-for-c-hook)
 (add-hook 'cuda-mode-hook common-function-for-c-hook)
 
-(setq load-path (cons "~/.emacs.d/plugins/yasnippet-0.6.1c" load-path))
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
-
-;;; Platform specific configuration
-
- (cond 
-  ((string-match "apple-darwin" system-configuration)
-   (autoload 'tuareg-mode "tuareg-mode")
-   (load "~/.emacs.d/plugins/html-helper-mode/html-helper-mode")
-   (setq load-path (cons "~/.emacs.d/plugins/haskell-mode-2.8.0" load-path))
-   )
-  ((string-match "mingw" system-configuration)
-   (require 'tramp)
-   (cons "/bin/sh" tramp-remote-path)
-   (setq load-path (cons "~/.emacs.d/plugins/haskell-mode-2.8.0" load-path))
-   (load "~/.emacs.d/plugins/html-helper-mode/html-helper-mode")
-   (modify-coding-system-alist 'process "plink" 'utf-8-unix)
-   (setq tramp-default-method "plink")))
-
-;;; tramp
-(setq tramp-default-user "kkato")
-
 ;;; Haskell-mode
-;(require 'inf-haskell)
-;(setq haskell-program-name "ghci")
 (load "haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;(add-hook 'haskell-mode-hook 'font-lock-mode)
-;(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
-
 (autoload 'ghc-init "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-;;; Screen size specific configuration
-
-(setq initial-frame-alist 
-      (append '((width . 85)
-		(height . 50)) initial-frame-alist))
-;(add-hook 'after-init-hook
-;	  (lambda () (split-window-horizontally) 
-;	    (other-window 0)))
-
-
-(setq c-default-style
-      '((java-mode . "gnu") ))
-
-;;; yatex
-(setq load-path (cons "~/.emacs.d/plugins/yatex1.75" load-path))
-
-;;; aspell
-(setq-default ispell-program-name "aspell")
-
-;;; ejacs
-(setq load-path (cons "~/.emacs.d/plugins/ejacs" load-path))
-(autoload 'js-console "js-console" nil t)
-
-;;; auto-install
-;(require 'auto-install)
-;(setq auto-install-directory "~/.emacs.d/auto-install/")
-;(auto-install-update-emacswiki-package-name t)
-;(auto-install-compatibility-setup)
-
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
