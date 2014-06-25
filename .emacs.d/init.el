d0f5744d4148f8ed2137fdf1bc59f9221f68fa13
Kimikazu Kato <kimikazu@gmail.com>
2014-06-25 16:50:58 +0900

    Settings for js3 changed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,7 +79,19 @@
 (add-hook 'python-mode-hook 
           (lambda () (local-set-key (kbd "C-m") 'newline-and-indent)))
 
-(setq js3-indent-level 2)
+;;; js3 mode
+(add-hook 'js3-mode-hook
+          (lambda () 
+            (setq js3-auto-indent-p t)
+            (setq js3-curly-indent-offset 0)
+            (setq js3-enter-indents-newline t)
+            (setq js3-expr-indent-offset 2)
+            (setq js3-indent-on-enter-key t)
+            (setq js3-lazy-commas t)
+            (setq js3-lazy-dots t)
+            (setq js3-lazy-operators t)
+            (setq js3-paren-indent-offset 2)
+            (setq js3-square-indent-offset 4)))
 
 ;; ess-mode
 (load "ess-site")
